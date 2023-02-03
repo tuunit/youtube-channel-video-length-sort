6c4b203e794bd6f32c909c7d465f95ef44b9b14d
tuunit <jan@larwig.com>
2023-02-04 00:38:12 +0100

    readd ascending code and fix page / url recognition error

--- content.js
+++ content.js
@@ -1,10 +1,4 @@
-"use-strict";
-
-
-console.log('The extension works');
-
 let state = true;
-
 const button = document.createElement("button");
 button.setAttribute("id", "duration-sort");
 button.innerText = "Duration (A)";
@@ -14,6 +8,7 @@ button.onclick = function () {
 }
 
 function ascending() {
+    l = [].map.call(document.querySelectorAll("span.ytd-thumbnail-overlay-time-status-renderer"), function (e) { l = e.innerHTML.trim().split(":").map(function (t) { return parseInt(t); }).reduce(function (p, c) { return p * 60 + c; }); return { a: e.parentElement.parentElement.parentElement, l: l }; }).sort(function (a, b) { return a.l - b.l; }); ctn = "YTD-CONTINUATION-ITEM-RENDERER"; i = document.querySelector("#primary #items"); if (i && i.childElementCount) { c = i.lastElementChild; if (c.tagName == ctn) { c.remove(); } else { c = null; } l.forEach(function (o) { i.appendChild(o.a.parentElement.parentElement.parentElement); }); if (c) i.appendChild(c); } else { i = document.querySelector("#primary #contents"); c = i.lastElementChild; if (c.tagName == ctn) { c.remove(); } else { c = null; } l.forEach(function (o) { i.appendChild(o.a.parentElement.parentElement.parentElement.parentElement.parentElement); }); if (c) i.appendChild(c); }
     state = false;
     button.innerText = "Duration (D)";
 }
@@ -25,9 +20,17 @@ function descending() {
 }
 
 function run() {
+    let url = window.location.href;
+    if (url.substring(url.lastIndexOf('/') + 1) != "videos") {
+        return;
+    }
+
     if (document.querySelector("#duration-sort")) {
         return;
     }
+
+    document.querySelector("#primary").style.display = 'none';
+    document.querySelector("#primary").style.display = 'block';
 
     const chips = document.querySelector("#scroll-container");
     chips.appendChild(button);
